2c06dbe2b339852f78d25837bd14292de8534345
djumanji <uludagc91@gmail.com>
2024-11-13 19:02:18 +0000

    Initilate DB at app startup

--- app.py
+++ app.py
@@ -2,6 +2,19 @@ from flask import Flask, request, redirect, render_template # type: ignore
 import sqlite3
 import re
 app = Flask(__name__)
+
+def init_db():
+    with sqlite3.conntect('email.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS subscribers (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                email TEXT NOT NULL UNIQUE
+            )
+        ''')
+        conn.commit()
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -12,8 +25,7 @@ def subscribe():
     if email and re.match(r"[^@]+@[^@]+\.[^@]+", email):
         try:
             with sqlite3.connect('email.db') as conn:
-                cursor = conn.cursor()
-                cursor.execute('CREATE TABLE IF NOT EXISTS subscribers (email TEXT)')
+                cursor.conn.cursor()
                 cursor.execute('INSERT INTO subscribers (email) VALUES (?)',(email,))
                 conn.commit()
             return redirect('/')
